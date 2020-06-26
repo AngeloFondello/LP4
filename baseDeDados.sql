5fdaa918170f242df40499b610a9180355e15452
srWerthmuller <srWerthmuller@users.noreply.github.com>
2020-06-26 16:22:27 -0300

    base de dados com inserts
    criada pelo Paulo

---
+++ baseDeDados.sql
@@ -0,0 +1,12 @@
+Create table concessionaria(
+id_veiculo integer primary key,
+nm_marca,
+dt_ano,
+ds_cor,
+vl_valor);
+
+insert into usuario(id_veiculo,nm_marca,dt_ano,ds_cor,vl_valor) values
+(1,'Fusca','1994','Preta',7500),
+(2,'Corsa','2000','Vermelho',9000),
+(3,'Uno','1998','Prata',8500),
+(4,'Fiesta','2001','Cinza',1000);
