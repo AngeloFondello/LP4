2909016ae90c8580d999f04c63da55f68101621e
srWerthmuller <srWerthmuller@users.noreply.github.com>
2020-06-26 16:35:10 -0300

    Update index.php

--- index.php
+++ index.php
@@ -4,21 +4,16 @@ $modelo = $_POST['modelo'] ?? null;
 $cor = $_POST['cor'] ?? null;
 $erro = null;
 
-if (isset($_COOKIE['login'])) { // verifica se tem cookie gravado
-    if ($login) { // SE FOI ENVIADO LOGIN PARA A PÁGINA
-        if ($login == $_COOKIE['login'] 
-                && $senha == $_COOKIE['senha']) {
-            // se login e senha forem iguais aos do cookie...
-
-            session_start(); // HABILITA O VETOR $_SESSION
-            $_SESSION['login'] = $login;
-            $_SESSION['senha'] = $senha;
-            header('LOCATION: protegida.php');
-        } else { // caso contrário, apresenta erros do acesso
+if (isset($_COOKIE['busca'])) { 
+    if ($busca) { 
+        if ($modelo == $_COOKIE['modelo'] 
+                && $cor == $_COOKIE['cor']) {
+           .
+        } else { 
             $erro = true;
         }
     }
-} else { // senão houver cookies, redireciona para a lista.
+} else { 
     header('LOCATION: lista.php');
 }
 ?>
@@ -33,11 +28,11 @@ if (isset($_COOKIE['login'])) { // verifica se tem cookie gravado
         <p>Carro não encontrado!</p>
         <?php endif; ?>
         <form method="post">
-            <p>Login: <br>
-                <input type="text" name="login"></p>
-            <p>Senha: <br>
-                <input type="password" name="senha"></p>
-            <p><input type="submit" value="Entrar"></p>
+            <p>Modelo: <br>
+                <input type="text" name="modelo"></p>
+            <p>Cor: <br>
+                <input type="text" name="cor"></p>
+            <p><input type="submit" value="Buscar"></p>
         </form>
     </body>
 </html>
